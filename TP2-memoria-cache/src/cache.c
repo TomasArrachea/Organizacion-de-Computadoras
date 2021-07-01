4d9e4054e502b4e4fb4c4779458aef7d99451090
tomasarrachea <tarrachea@fi.uba.ar>
2021-07-01 03:40:44 -0300

    lectura de archivos y argumentos

--- TP2-memoria-cache/src/cache.c
+++ TP2-memoria-cache/src/cache.c
@@ -3,48 +3,110 @@
 //politica de reemplazo FIFO y escritura WT/¬WA
 //espacio de direcciones de 16 bits
 //cada bloque de cache cuenta con su metadata, incluyendo bit V y tag, y campo que permita implementar la politica FIFO.
+#include <stdlib.h>
 
+//HACER UN TYPEDEF UNSIGNED SHORT INT y uniformizar las conversiones de tipos entre int y short int
 
-typedef struct memoria_16_bits {
-	const unsigned int CAPACIDAD = 0xffff // 65535 bytes
-	bloque_t bloques[CAPACIDAD/tam_bloque];
-} memoria_t;
+typedef struct bloque {
+	char* datos;
+	char valido; //QUE SIGNIFICA? los bloques que estan en cache son validos? o solo los bloques que tienen un valor?
+	char tag;
+} bloque_t;
 
 typedef struct cache {
 	unsigned short int vias;
 	unsigned short int capacidad;
 	unsigned short int tam_bloque;
 	
-	unsigned short int tam_set = tam_bloque * vias;
-	unsigned short int cant_sets = capacidad / (tam_bloque * vias);
-	unsigned short int cant_bloques =  capacidad / tam_bloque;
+	unsigned short int cant_sets;
+	unsigned short int cant_bloques;
 
-	bloque_t bloques[cant_bloques];
-	unsigned short int topes[cant_sets];
+	bloque_t* bloques;
+	unsigned short int* topes;
 	unsigned short int accesos;
 	unsigned short int misses;
 } cache_t;
 
-typedef struct bloque {
-	unsigned int datos[tam_bloque];
-	char valido;
-	char tag;
-} bloque_t;
+typedef struct memoria_16_bits {
+	unsigned int capacidad; // 65535 bytes, CONSTANTE
+	bloque_t* bloques;
+} memoria_t;
+
+
+cache_t cache;
+memoria_t memoria;
+
+
+bloque_t new_bloque(int tamanio, char tag) {
+	bloque_t bloque;
+	bloque.valido = 0; //son validos los bloques de la memoria? no y se validan cuando se les escribe un dato
+	bloque.tag = tag;
+	bloque.datos = malloc(tamanio * sizeof(char));
+	// for (int i = 0; i < tamanio; ++i) { bloque.datos[i] = 0; } hace falta inicializar en 0 todos los datos? podrian ser basura invalida
+	return bloque;
+}
+
+void init_memoria() {
+	int cant_bloques_memoria = (memoria.capacidad / cache.tam_bloque);
+	memoria.capacidad = 0xffff;
+	memoria.bloques = malloc(cant_bloques_memoria * sizeof(bloque_t));
+
+	for (int i = 0; i < cant_bloques_memoria; i++) {
+		memoria.bloques[i] = new_bloque(cache.tam_bloque, i / cache.vias);
+	}
+}
+
+void init_cache(short unsigned int vias, short unsigned int capacidad, short unsigned int tam_bloque) {
+	cache.vias = vias;
+	cache.capacidad = capacidad;
+	cache.tam_bloque = tam_bloque;
+
+	cache.cant_sets = capacidad / (tam_bloque * vias);
+	cache.cant_bloques = capacidad / tam_bloque;
+
+	cache.bloques = malloc(cache.cant_bloques * sizeof(bloque_t));
+	for (int i = 0; i < cache.cant_bloques; i++) {
+		cache.bloques[i] = new_bloque(tam_bloque, i / cache.vias);
+		// cache.bloques[i].valido = 0; // no hay bloques inicializados, SEGFAULT?
+	}
+	for (int i = 0; i < cache.cant_sets; i++) {
+		cache.topes[i] = 0;
+	}
+	cache.misses = 0;
+	cache.accesos = 0;
+
+	init_memoria();
+}
 
 // inicializar los bloques de la caché como inválidos, la memoria simulada en 0 y la tasa de misses a 0
 void init() {
-	//metadatos del cache ya inicializados? vias, capacidad, cant_sets, topes
-	for (int i = 0; i < cache.cant_sets; i++) {
-		tope[i] = 0;
-	}
 	for (int i = 0; i < cache.cant_bloques; i++) {
 		cache.bloques[i].valido = 0;
 	}
 	cache.misses = 0;
 	cache.accesos = 0;
 
-	//inicializar memoria en 0? podria iterar todas las posiciones, vale la pena? Podria generar bloques en cero mas facil y cargarlos.
-} 
+	int cant_bloques_memoria = (memoria.capacidad / cache.tam_bloque);
+	for (int i = 0; i < cant_bloques_memoria; i++) {
+		memoria.bloques[i].valido = 0;
+		for (int j = 0; j < cache.tam_bloque; j++) {
+			memoria.bloques[i].datos[j] = 0;
+		}
+	}
+}
+
+void delete() {
+	int cant_bloques_memoria = (memoria.capacidad / cache.tam_bloque);
+	for (int i = 0; i < cant_bloques_memoria; i++) {
+		free(memoria.bloques[i].datos);
+	}
+	free(memoria.bloques);
+
+	for (int i = 0; i < cache.cant_bloques; i++) {
+		free(cache.bloques[i].datos);
+	}
+	free(cache.bloques);
+}
 
 //devolver el conjunto de caché al que mapea la dirección address.
 unsigned int find_set(int address) {
@@ -59,6 +121,7 @@ unsigned int find_earliest(int setnum) {
 
 //leer el bloque blocknum de memoria y guardarlo en el lugar que le corresponda en la memoria caché
 void read_block(int blocknum) {
+	memoria.bloques[blocknum].valido = 1;
 	unsigned int setnum = find_set(blocknum * cache.tam_bloque);
 	cache.bloques[find_earliest(setnum)] = memoria.bloques[blocknum];
 
@@ -76,13 +139,13 @@ void write_byte_tomem(int address, char value) {
 }
 
 /*usar tag para ver si cache tiene el bloque correspondiente a la address*/
-// devuelve el indice del bloque en el array de bloques (la posicion en cache).
-int find_block(int setnum, char* hit){
+// devuelve el indice del bloque en el array de bloques (la posicion en cache) si es un hit, si no devuelve basura.
+unsigned short int find_block(int setnum, char* hit){
 	short int i = 0, blocknum;
 	*hit = 0;
 	while (!(*hit) && i < cache.vias) {
-		blocknum = setnum * vias + i;
-		if (cache.bloques[blocknum].tag = setnum) {
+		blocknum = setnum * cache.vias + i;
+		if (cache.bloques[blocknum].tag == setnum) {
 			*hit = 1;
 		}
 	}
@@ -93,35 +156,36 @@ int find_block(int setnum, char* hit){
 //escribir 1 en *hit si es un hit y 0 si es un miss
 //sólo debe interactuar con la memoria a través de las otras primitivas.
 char read_byte(int address, char *hit) {
-	find_block(find_set(address));
-	
-	if *hit == 0
+	unsigned short int blocknum = find_block(find_set(address), hit);
+	if (*hit == 0) {
 		read_block(address / cache.tam_bloque);
-
-	unsigned short int mascara = (0xffff / cache.tam_bloque) & ~(0xffff / cache.cant_sets);
-	unsigned short int set_offset = address & mascara;
-	unsigned short int setnum = find_set(address) * vias;
-	return cache.bloques[setnum + set_offset];
+		blocknum = find_block(find_set(address), hit);
+		cache.misses++;
+	}
+	cache.accesos++;
+	unsigned int block_offset = address & ~(0xffff / cache.cant_bloques); // si la cant de bloques no es potencia de 2 pasan cosas raras, es mejor usar >> log2(cant_bloques), o ver que pasa usando %, como find_set
+	return cache.bloques[blocknum].datos[block_offset];
 }
 
-//escribir el valor value en la posición correcta del bloque que corresponde a address, en la memoria y, si está, en el caché
+// escribir el valor value en la posición correcta del bloque que corresponde a address, si está en el caché, y en la memoria en todos los casos, y
 //devolver 1 si es un hit y 0 si es un miss.
 //sólo debe interactuar con la memoria a través de las otras primitivas.
-char write_byte(int address, char value, char *hit){
-	int blocknum = find_block(find_set(address));
+char write_byte(int address, char value){
+	char hit;
+	unsigned short int blocknum = find_block(find_set(address), &hit);
+	cache.accesos++;
 
-	if (*hit == 1) {
-		unsigned short int mascara = (0xffff / cache.tam_bloque) & ~(0xffff / cache.cant_sets);
-		unsigned short int set_offset = address & mascara;
-		unsigned short int setnum = find_set(address) * vias;
+	if (hit == 1) {
 		unsigned int block_offset = address & ~(0xffff / cache.cant_bloques); // si la cant de bloques no es potencia de 2 pasan cosas raras, es mejor usar >> log2(cant_bloques), o ver que pasa usando %, como find_set
-		cache.bloques[setnum + set_offset].datos[block_offset] = value;
-	}
+		cache.bloques[blocknum].datos[block_offset] = value;
+	} else
+		cache.misses++;
+
 	write_byte_tomem(address, value);
+	return hit;
 }
 
 //devolver el porcentaje de misses desde que se inicializó el cache
 char get_miss_rate() {
-	cache.misses / (cache.accesos);
+	return cache.misses / cache.accesos;
 }
-
--- TP2-memoria-cache/src/tp2.c
+++ TP2-memoria-cache/src/tp2.c
@@ -2,53 +2,131 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <ctype.h>
+#include <getopt.h>
 
 const char* VERSION_ACTUAL = "1.0";
-cache_t cache;
 
 void imprimir_ayuda();
 void imprimir_version();
+void leer_archivo(char* nombre);
+void ejecutar_instrucciones(char* archivo_instrucciones, char* archivo_salida);
+void parse_arguments(int argc, char** argv, int* ways, int* cache_size, int* block_size, char* archivo_salida, char* archivo_instrucciones);
 
 int main(int argc, char* argv[]) {
 	FILE* salida = NULL;
-	int c, ways, cache_size, block_size;
+	int ways = 4, cache_size = 4096, block_size = 256;
+	char* archivo_salida = "resultado_cache.mem", *archivo_instrucciones;
 
-	while ((c = getopt(argc, argv, "hVowcsbs:")) != -1)
+	parse_arguments(argc, argv, &ways, &cache_size, &block_size, archivo_salida, archivo_instrucciones);
+	init_cache(ways, cache_size, block_size);
+    ejecutar_instrucciones(archivo_instrucciones, archivo_salida);
+    delete();
+	return 0;
+}
+
+
+void parse_arguments(int argc, char** argv, int* ways, int* cache_size, int* block_size, char* archivo_salida, char* archivo_instrucciones) {
+	int c;
+    static struct option long_options[] = {
+        {"help",    no_argument,      0,  'h' },
+        {"version",	no_argument,       0,  'v' },
+        {"output",  required_argument, 0,  'o' },
+        {"ways",	required_argument, 0,  'w' },
+        {"cachesize", required_argument, 0,  'c' },
+        {"blocksize", required_argument, 0,  'b' },
+        {"cs", required_argument, 0,  'c' },
+        {"bs", required_argument, 0,  'b' },
+        {0,           0,                 0,  0   }
+    };
+
+	while ((c = getopt_long_only(argc, argv, "hVo:w:c:b:", long_options, NULL)) != -1) //usar longopt para poder manear opciones de varios caracteres
 		switch (c) {
 			case 'h':
 				imprimir_ayuda();
-			    return 0;
+			    return;
 			case 'V':
 				imprimir_version();
-			    return 0;
+			    return;
 			case 'o':
-			    nombre_salida = optarg;
+			    archivo_salida = optarg;
 			    break;
 			case 'w':
-			    ways = optarg;
+			    *ways = atoi(optarg);
 			    break;
-			case 'cs':
-			    cache_size = optarg;
+			case 'c':
+			    *cache_size = atoi(optarg);
 			    break;
-			case 'bs':
-			    block_size = optarg;
+			case 'b':
+			    *block_size = atoi(optarg);
 			    break;
 			case '?':
-			    if (optopt == 'o')
+			    if (optopt == 'o' || optopt == 'c' || optopt == 'b' || optopt == 'w')
 					fprintf (stderr, "La opción -%c requiere un argumento.\n", optopt);
 			    else if (isprint (optopt))
 			    	fprintf (stderr, "Opción desconocida `-%c'.\n", optopt);
 			    else
 			    	fprintf (stderr,"Caracter desconocido `\\x%x'.\n", optopt);
-			    return 1;
+			    return;
 			default:
-			    abort ();
-    }  
+				abort();
+				// fprintf (stderr, "Error: los argumentos son inválidos. Ingrese -h para ver la ayuda.")
+    }
     int index = optind;
+    if (argc < 2) {	
+		fprintf (stderr, "Error: se debe ingresar el nombre de un archivo con instrucciones. Ingrese -h para ver la ayuda.");
+		return;
+    }
+	archivo_instrucciones = argv[index++];
+}
 
-    init();
+void ejecutar_instrucciones(char* archivo_instrucciones, char* archivo_salida) {
+	FILE* instrucciones = fopen(archivo_instrucciones, "r");
+	if (!instrucciones){
+		fprintf(stderr, "Error al abrir el archivo de instrucciones.");
+		return;
+	}
+	FILE* salida = fopen(archivo_salida, "r");
+	if (!salida){
+		fprintf(stderr, "Error al abrir el archivo de resultados.");
+		return;
+	}
 
-	return 0;
+	char comando;
+	while(!feof(instrucciones)) {
+		fscanf(instrucciones, "%c", &comando);
+		switch (comando) {
+			case 'W' : {
+				int address;
+				char value;
+				fscanf(instrucciones, "%i, %c\n", &address, &value);
+				char hit = write_byte(address, value);
+				fprintf(salida, "Write value %c in address %i: hit = %c\n", value, address, hit);
+				break;
+			}
+			case 'R': {
+				int address;
+				fscanf(instrucciones, "%i\n", &address);
+				char hit;
+				char value = read_byte(address, &hit);
+				fprintf(salida, "Read value %c from address %i: hit = %c\n", value, address, hit);
+				break;
+			}
+			case 'M':
+				fscanf(instrucciones, "R\n");
+				fprintf(salida, "Miss rate: %i\n", get_miss_rate());
+				break;
+			case 'i':
+				fscanf(instrucciones, "nit\n");
+				init();
+				fprintf(salida, "init\n");
+				break;
+			default:
+				return;
+		}
+	}
+
+	fclose(instrucciones);
+	fclose(salida);
 }
 
 void imprimir_ayuda(){
